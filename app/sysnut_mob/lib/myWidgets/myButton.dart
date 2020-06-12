241735ccd133be1aed9d4ba6659b6f0f08fd9975
Felipe-dot <felipeluis610@gmail.com>
2020-06-12 17:01:14 -0300

    melhora do button

--- app/sysnut_mob/lib/myWidgets/myButton.dart
+++ app/sysnut_mob/lib/myWidgets/myButton.dart
@@ -5,37 +5,41 @@ class MyButton extends StatelessWidget {
   final IconData myIcon;
   final Widget myPage;
 
-  MyButton(this.name, this.myIcon,this.myPage);
+  MyButton(this.name, this.myIcon, this.myPage);
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      elevation: 10,
-      child: Column(
-        children: <Widget>[
-          Icon(
-            myIcon,
-            size: 25,
-            color: Colors.white,
-          ),
-          Text(
-            name,
-            style: TextStyle(
-              fontSize: 25,
-              fontFamily: "Dancing Script",
+    return ButtonTheme(
+      height: 145,
+      minWidth: 150,
+      child: RaisedButton(
+        elevation: 10,
+        child: Column(
+          children: <Widget>[
+            Icon(
+              myIcon,
+              size: 25,
               color: Colors.white,
             ),
-          ),
-        ],
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 25,
+                fontFamily: "Dancing Script",
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => myPage,
+            ),
+          );
+        },
       ),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => myPage,
-          ),
-        );
-      },
     );
   }
 }
